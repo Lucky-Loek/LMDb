c54303b4af7f2c9a7d6b2be681c527fae66a6784
Loek van der Linde <lind0096@hz.nl>
2019-03-20 11:20:44 +0100

    Add UsersTableSeeder to array so it actually seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(TypesTableSeeder::class);
+         $this->call([
+             TypesTableSeeder::class,
+             UserTableSeeder::class
+         ]);
     }
 }
