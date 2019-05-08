7f8fe36c906af87f882f88d138c46bdc243655d0
Loek van der Linde <lind0096@hz.nl>
2019-05-08 17:08:50 +0200

    Add first working version of screening importer

--- app/Service/ApiService.php
+++ app/Service/ApiService.php
@@ -2,7 +2,13 @@
 
 namespace App\Service;
 
+use App\Actor;
 use App\Client\Client;
+use App\Director;
+use App\Genre;
+use App\Screening;
+use App\Type;
+use App\Writer;
 
 class ApiService
 {
@@ -16,31 +22,68 @@ class ApiService
         $this->client = $client;
     }
 
-    public function ietsofzo()
+    public function processImdbElements(array $imdbElements): void
     {
-        $response = $this->client->getScreeningById('tt0111161');
+        $counter = 0;
 
-        $decoded = json_decode($response);
+        foreach ($imdbElements as $imdbElement) {
+            $response = $this->client->getScreeningByImdbId($imdbElement['Const']);
+            $decoded = json_decode($response);
 
+
+            $type = Type::where('type', $decoded->Type)->first();
+            $screening = $this->createScreeningFromResponse($decoded, $type);
+
+            dump($screening->id, $screening->title);
+
+            if($counter === 900) {
+                exit;
+            }
+
+            $counter++;
+        }
+    }
+
+    private function createScreeningFromResponse(\StdClass $decoded, Type $type): Screening
+    {
         $actors = $this->createAndGetItemsFromString($decoded->Actors, Actor::class);
         $directors = $this->createAndGetItemsFromString($decoded->Director, Director::class);
         $genres = $this->createAndGetItemsFromString($decoded->Genre, Genre::class);
-        $type = Type::where('type', $decoded->Type);
+        $writers = $this->createAndGetItemsFromString($decoded->Writer, Writer::class);
+
+
+        $screening = Screening::create([
+            'title' => $decoded->Title,
+            'year' => $decoded->Year,
+            'runtime' => $decoded->Runtime,
+            'poster_file_path' => $decoded->Poster, // #TODO change to getting actual image and writing to filesystem, including thumbnail
+            'imdb_rating' => $decoded->imdbRating,
+            'imdb_id' => $decoded->imdbID,
+            'type_id' => $type->id,
+            'count' => 1,
+        ]);
+
+        $screening->actors()->saveMany($actors);
+        $screening->directors()->saveMany($directors);
+        $screening->genres()->saveMany($genres);
+        $screening->writers()->saveMany($writers);
+
+        $screening->save();
+
+        return $screening;
     }
 
     private function createAndGetItemsFromString(string $items, string $className): array
     {
+        $items = preg_replace("/\([^)]+\)/", "", $items);
         $itemNames = explode(', ', $items);
 
         $items = [];
         foreach ($itemNames as $name) {
+            $name = trim($name);
             $items[] = $className::firstOrCreate(['name' => $name]);
         }
 
         return $items;
-    }
-
-    public function processImdbElements(array $imdbElements)
-    {
     }
 }
