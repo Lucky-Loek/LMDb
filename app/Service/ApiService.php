6378fbe739f5baeafc9b39e9816edb68bcf95e9e
Loek van der Linde <lind0096@hz.nl>
2019-07-01 14:41:48 +0200

    Refactor import service and add poster image handling

--- app/Service/ApiService.php
+++ app/Service/ApiService.php
@@ -2,13 +2,7 @@
 
 namespace App\Service;
 
-use App\Actor;
 use App\Client\Client;
-use App\Director;
-use App\Genre;
-use App\Screening;
-use App\Type;
-use App\Writer;
 
 class ApiService
 {
@@ -22,68 +16,9 @@ class ApiService
         $this->client = $client;
     }
 
-    public function processImdbElements(array $imdbElements): void
+    public function processImdbElement(array $imdbElement): \StdClass
     {
-        $counter = 0;
-
-        foreach ($imdbElements as $imdbElement) {
-            $response = $this->client->getScreeningByImdbId($imdbElement['Const']);
-            $decoded = json_decode($response);
-
-
-            $type = Type::where('type', $decoded->Type)->first();
-            $screening = $this->createScreeningFromResponse($decoded, $type);
-
-            dump($screening->id, $screening->title);
-
-            if($counter === 900) {
-                exit;
-            }
-
-            $counter++;
-        }
-    }
-
-    private function createScreeningFromResponse(\StdClass $decoded, Type $type): Screening
-    {
-        $actors = $this->createAndGetItemsFromString($decoded->Actors, Actor::class);
-        $directors = $this->createAndGetItemsFromString($decoded->Director, Director::class);
-        $genres = $this->createAndGetItemsFromString($decoded->Genre, Genre::class);
-        $writers = $this->createAndGetItemsFromString($decoded->Writer, Writer::class);
-
-
-        $screening = Screening::create([
-            'title' => $decoded->Title,
-            'year' => $decoded->Year,
-            'runtime' => $decoded->Runtime,
-            'poster_file_path' => $decoded->Poster, // #TODO change to getting actual image and writing to filesystem, including thumbnail
-            'imdb_rating' => $decoded->imdbRating,
-            'imdb_id' => $decoded->imdbID,
-            'type_id' => $type->id,
-            'count' => 1,
-        ]);
-
-        $screening->actors()->saveMany($actors);
-        $screening->directors()->saveMany($directors);
-        $screening->genres()->saveMany($genres);
-        $screening->writers()->saveMany($writers);
-
-        $screening->save();
-
-        return $screening;
-    }
-
-    private function createAndGetItemsFromString(string $items, string $className): array
-    {
-        $items = preg_replace("/\([^)]+\)/", "", $items);
-        $itemNames = explode(', ', $items);
-
-        $items = [];
-        foreach ($itemNames as $name) {
-            $name = trim($name);
-            $items[] = $className::firstOrCreate(['name' => $name]);
-        }
-
-        return $items;
+        $response = $this->client->getScreeningByImdbId($imdbElement['Const']);
+        return json_decode($response);
     }
 }
--- app/Service/ImportService.php
+++ app/Service/ImportService.php
@@ -3,10 +3,11 @@
 namespace App\Service;
 
 use App\Actor;
-use App\Client\Client;
 use App\Director;
 use App\Genre;
+use App\Screening;
 use App\Type;
+use App\Writer;
 
 class ImportService
 {
@@ -34,6 +35,90 @@ class ImportService
         $imdbElements = $this->csvReaderService->readWatchlist();
 
         // Run line for line through api
-        $this->apiService->processImdbElements($imdbElements);
+        foreach ($imdbElements as $imdbElement) {
+
+            // Check if already in database
+            $screening = Screening::whereImdbId($imdbElement['Const'])->first();
+
+            if (empty($screening)) {
+                $processedElement = $this->apiService->processImdbElement($imdbElement);
+
+                $type = Type::where('type', $processedElement->Type)->first();
+                $screening = $this->createScreeningFromResponse($processedElement, $type);
+
+                $message = 'Inserted screening with id "' . $screening->id . '" and title "' . $screening->title . '"';
+                \Log::info($message);
+                dump($message);
+            } else {
+                $message = 'Already found screening with id "' . $screening->id . '" and title "' . $screening->title . '"';
+                \Log::info($message);
+                dump($message);
+            }
+        }
+    }
+
+    private function createScreeningFromResponse(\StdClass $decoded, Type $type): Screening
+    {
+        $actors = $this->createAndGetItemsFromString($decoded->Actors, Actor::class);
+        $directors = $this->createAndGetItemsFromString($decoded->Director, Director::class);
+        $genres = $this->createAndGetItemsFromString($decoded->Genre, Genre::class);
+        $writers = $this->createAndGetItemsFromString($decoded->Writer, Writer::class);
+
+        $posterFilePaths = $this->savePosterAndReturnFilePaths($decoded->Poster, $decoded->Title);
+
+        $screening = Screening::create([
+            'title' => $decoded->Title,
+            'year' => $decoded->Year,
+            'runtime' => $decoded->Runtime,
+            'poster_file_path' => $posterFilePaths['file_path'],
+            'poster_thumbnail_file_path' => $posterFilePaths['thumbnail_file_path'],
+            'imdb_rating' => $decoded->imdbRating,
+            'imdb_id' => $decoded->imdbID,
+            'type_id' => $type->id,
+            'count' => 1,
+        ]);
+
+        $screening->actors()->saveMany($actors);
+        $screening->directors()->saveMany($directors);
+        $screening->genres()->saveMany($genres);
+        $screening->writers()->saveMany($writers);
+
+        $screening->save();
+
+        return $screening;
+    }
+
+    private function createAndGetItemsFromString(string $items, string $className): array
+    {
+        // Remove all non-name items, like "(Produced by) from the names"
+        $items = preg_replace("/\([^)]+\)/", "", $items);
+        $itemNames = explode(', ', $items);
+
+        $items = array_map(function($name) use ($className) {
+            return $className::firstOrCreate(['name' => trim($name)]);
+        }, $itemNames);
+
+        return $items;
+    }
+
+    private function savePosterAndReturnFilePaths(string $posterUrl, string $screeningTitle): array
+    {
+        $image = \Image::make($posterUrl);
+
+        // Replace all weird characters but digits and alphabetic characters
+        $screeningTitle = strtolower($screeningTitle);
+        $screeningTitle = preg_replace('/[^\da-z]/', '_', $screeningTitle);
+
+        $filePath = storage_path('app/public/') . $screeningTitle . '.jpg';
+        $thumbnailFilePath = storage_path('app/public/') . $screeningTitle . '_thumb.jpg';
+
+        $image->save($filePath);
+        $image = $image->widen(180);
+        $image->save($thumbnailFilePath);
+
+        return [
+            'file_path' => $filePath,
+            'thumbnail_file_path' => $thumbnailFilePath
+        ];
     }
 }
