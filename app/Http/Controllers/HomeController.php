720c1d73cea42df1ebe78e2d5a776e7f35b20124
Loek van der Linde <lind0096@hz.nl>
2019-07-03 15:23:58 +0200

    Remove authentication from home view and replace it with posters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Screening;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+
     }
 
     /**
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $screenings = Screening::limit(18)->get();
+        return view('home', compact('screenings'));
     }
 }
