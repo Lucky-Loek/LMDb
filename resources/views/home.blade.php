ccf81b256799f70c6aa8fb90464992304a842ef0
Loek van der Linde <lind0096@hz.nl>
2019-07-03 15:24:28 +0200

    Create a poster grid on the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,44 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    {{-- Styles --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    {{-- JavaScript --}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <title>Homepage</title>
+</head>
+<body>
+<section class="section">
+    <div class="container">
+        <div class="tile is-ancestor">
+
+            @foreach($screenings->chunk(3) as $chunk)
+                <div class="tile is-parent is-2 is-vertical">
+
+                    @foreach($chunk as $screening)
+                        <div class="tile is-child">
+                            <article class="tile is-child card">
+                                <div class="card-image">
+                                    <figure class="image is-3by4">
+                                        <img src="{{ 'storage/' . $screening->poster_thumbnail_file_path }}" alt="Placeholder image">
+                                    </figure>
+                                </div>
+                            </article>
                         </div>
-                    @endif
+                    @endforeach
 
-                    You are logged in!
                 </div>
-            </div>
+            @endforeach()
+
         </div>
     </div>
-</div>
-@endsection
+</section>
+</body>
+</html>
